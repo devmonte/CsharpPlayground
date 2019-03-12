7e97d963d02feff20121abcfd33e6c255b367dcc
monte <greg.jonca@gmail.com>
2019-03-12 07:04:50 +0100

    Add class to show nullable reference

---
+++ CSharpEightFeatures/NullableReference.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEightFeatures
+{
+    public class NullableReference
+    {
+        private string _firstSentence;
+        private string? _secondSentence;
+
+        public void TryNullable()
+        {
+            _firstSentence = null;
+            _secondSentence = null;
+            string thirdSentence = null;
+
+            Console.WriteLine(_firstSentence.Length);
+            Console.WriteLine(_secondSentence.Length);
+            Console.WriteLine(thirdSentence.Length);
+
+
+        }
+    }
+}
