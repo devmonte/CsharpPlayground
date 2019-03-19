853f8e8088b1f1df9ca5a0dc4e48346fdc5aec45
Grzegorz Jonca <grzegorz.jonca@squaretec.net>
2019-03-19 11:17:54 +0100

    dd basic logic for demonstrate inheritance

---
+++ Varieties/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varieties
+{
+    public class Animal //base Class
+    {
+        public void Move()
+        {
+            Console.WriteLine("Moving...");
+        }
+
+        public void Move(int howLong)
+        {
+            Console.WriteLine("Moving..." + howLong);
+        }
+
+        public virtual void Move(string destination)
+        {
+            Console.WriteLine("Moving..." + destination);
+        }
+
+    }
+}
---
+++ Varieties/Monkey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varieties
+{
+    public class Monkey : Animal
+    {
+        public override void Move(string destination)
+        {
+            base.Move(destination);
+            Console.WriteLine("Overriden!");
+        }
+    }
+}
