748dba2c28de35184cb989e35d16d6818b4934db
Grzegorz Jonca <grzegorz.jonca@squaretec.net>
2019-04-09 14:17:43 +0200

    Play with exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AppDomain.CurrentDomain.FirstChanceException += (sender, eventArgs) =>
+            {
+                Console.WriteLine($"{sender}, {eventArgs}");
+            };
+            AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
+            {
+                Console.WriteLine($"{sender}, {eventArgs}");
+            };
+
+            try
+            {
+                //throw new NotImplementedException();
+                var t = new Thread(() => throw new NotImplementedException());
+                t.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
