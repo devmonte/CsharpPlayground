186e754c0938ab0217700b3a37ca66bac24d09ef
monte <greg.jonca@gmail.com>
2019-04-16 22:06:44 +0200

    Add project for delegates and lambdas

---
+++ DelegatesAndLambdas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DelegatesAndLambdas
+{
+    class Program
+    {
+        public delegate void TestDelegate(string name);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestDelegate test = PrintName;
+            test += PrintSecondName;
+            test("Greg");
+            test += (s) => { Console.WriteLine($"Hello {s}"); };
+            test = (d) => { Console.WriteLine($"Hello {d}"); };
+            test("Tom!");
+
+            Console.ReadKey();
+        }
+
+        static void PrintName(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        static void PrintSecondName(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+}
