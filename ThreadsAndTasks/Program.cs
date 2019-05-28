b92a95703577528cc314d10ca21e58d8dd74d247
Grzegorz Jonca <grzegorz.jonca@squaretec.net>
2019-05-28 16:41:19 +0200

    Working on tasks and threads

---
+++ ThreadsAndTasks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsAndTasks
+{
+    class Program
+    {
+        private static readonly object Guard = new object();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Start {Thread.CurrentThread.ManagedThreadId}");
+            var t1 = new Thread(DoSomethingImportant);
+            var t2 = new Thread(DoSomethingImportant);
+
+            t1.Start();
+            t2.Start();
+
+            Console.WriteLine("End of World!");
+            Console.ReadKey();
+        }
+
+        static void DoSomethingImportant()
+        {
+            lock (Guard)
+            {
+                Console.WriteLine($"Important stuff, {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
