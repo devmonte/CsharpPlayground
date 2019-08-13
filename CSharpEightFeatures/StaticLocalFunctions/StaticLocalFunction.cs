d94817e831782b2cf6c3e667d9677dd04ee581d3
devmonte <greg.jonca@gmail.com>
2019-08-13 10:21:14 +0200

    Add static local function example

---
+++ CSharpEightFeatures/StaticLocalFunctions/StaticLocalFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEightFeatures.StaticLocalFunctions
+{
+    public class StaticLocalFunction
+    {
+        public int DoSomeCalculation(int a, int b)
+        {
+            return LocalSum();
+            int LocalSum() => a + b;
+        }
+
+        public int DoSomeCalculationWithStaticLocalFunc(int a, int b)
+        {
+            return LocalSum(a, b);
+            static int LocalSum(int x, int y) => x + y;
+        }
+    }
+}
