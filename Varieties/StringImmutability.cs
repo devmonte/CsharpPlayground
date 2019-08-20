1f8df75d35584abd079b1da9da424736030d2337
devmonte <greg.jonca@gmail.com>
2019-08-20 12:40:27 +0200

    Add small example of string immutability

---
+++ Varieties/StringImmutability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varieties
+{
+    public class StringImmutability
+    {
+        public void Show()
+        {
+            string test = "TestValue";
+            test = "AnotherValue"; //it's OK, but new object(array) is created
+            //test[0] = 'z';  cause error, it's readonly 
+        }
+    }
+}
