94e4bef1572269bc85a807bc251c37bf8e6497d6
devmonte <greg.jonca@gmail.com>
2019-08-26 12:00:26 +0200

    Add culture example

--- Varieties/Program.cs
+++ Varieties/Program.cs
@@ -6,12 +6,18 @@ namespace Varieties
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start varieties example!");
-
+            Console.WriteLine("Start play with culture example!");
             new PlayWithCulture().Demo();
+            Console.WriteLine("End of play with culture example!");
+
+            Console.WriteLine("Start reference type example!");
+            var myCar = new Car { Name = "MyCarName" };
+            new ReferenceTypes().ChangeCarName(myCar);
+            Console.WriteLine(myCar.Name);
+            Console.WriteLine("End of reference type example!");
 
 
-            Console.WriteLine("End of examples!");
+
             Console.ReadKey();
         }
     }
---
+++ Varieties/ReferenceTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varieties
+{
+    public class Car
+    {
+        public string Name { get; set; }
+    }
+
+    public class ReferenceTypes
+    {
+        public void ChangeCarName(Car car)
+        {
+            car.Name = "ChangedName";
+            car = new Car { Name = "NAmeInNewCarInstance" };
+        }
+
+    }
+}
